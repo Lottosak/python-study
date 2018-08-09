19d9938be182094e9b5b910e68aad752426a8eed
Lottosak <40791314+Lottosak@users.noreply.github.com>
2018-08-09 12:28:00 +0200

    simple bussiness logic

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,36 @@
 import json
+#from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 print("Welcome to dictionary app")
 
-data = json.load(open("data.json"))
+data = json.load(open("data.json"))        
+
+def getDefinition(word):
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        closest = get_close_matches(word, data.keys())[0]
+        flag = input("Did you mean this word %s? (Y/N)" % closest)
+        if flag.lower() == "y":
+            return data[closest]
+        elif flag.lower() == "n":
+            return "Word doesn't exist"
+        else:
+            return "Lern to write Y or N :)"
+    else:
+        return "Unknown word"
+
+def nicePrint(val):
+    if type(val) == list:
+        num = 1
+        for item in val:
+            print("%s. %s" % (num,item))
+            num += 1
+    else:
+        print(val)
 
 word = input("Write word you want to find: ")
-
-if word in data:
-    print(data[word])
-else:
-    print("Incorect word")
+nicePrint(getDefinition(word.lower()))
 
 
